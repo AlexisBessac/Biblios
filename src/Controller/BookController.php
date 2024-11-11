6e78cab815089ed1d3fb52686e024b929d1f898f
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-11-11 15:31:30 +0100

    Mise en place finale de la pagination pour le catalogue des livres

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,18 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Books;
 use App\Repository\BooksRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class BookController extends AbstractController
 {
-    
     #[Route('/book', name: 'app_book_index')]
-    public function index(BooksRepository $booksRepository): Response
+    public function index(BooksRepository $booksRepository, PaginatorInterface $paginator, Request $request): Response
     {
-       
-        $books = $booksRepository->findAll();
+        // Utiliser une requête Doctrine ou une QueryBuilder pour la pagination
+        $query = $booksRepository->createQueryBuilder('b')->getQuery();
+
+        // Paginer les résultats
+        $books = $paginator->paginate(
+            $query, // La requête ou QueryBuilder à paginer
+            $request->query->getInt('page', 1), // Page actuelle, par défaut 1
+            4 // Nombre d'éléments par page
+        );
 
         return $this->render('book/index.html.twig', [
             'books' => $books,
