5f90864a4feb621cbf5899da2b0e50400fdc561e
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-11-11 14:51:00 +0100

    Mise en place de la pagination

--- src/Repository/BooksRepository.php
+++ src/Repository/BooksRepository.php
@@ -5,15 +5,27 @@ namespace App\Repository;
 use App\Entity\Books;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\Paginator;
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @extends ServiceEntityRepository<Books>
  */
 class BooksRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private PaginatorInterface $paginator)
     {
         parent::__construct($registry, Books::class);
+    }
+
+    public function paginatorBooks(int $page, int $limit) : PaginationInterface
+    {
+        return $this->paginator->paginate(
+            $this->createQueryBuilder('b'),
+            $page,
+            $limit
+        );
     }
 
     //    /**
