4785cfa01eb3db1fd1fe53415a013f8f3e676f12
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-12-13 16:07:06 +0100

    Fin du CSS du CRUD des livres, catalogue des livres et affichages des détails

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Books;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\BooksRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class BookController extends AbstractController
 {
     #[Route('/book', name: 'app_book_index')]
-    public function index(): Response
+    public function index(BooksRepository $bookRepository, Request $request): Response
     {
         return $this->render('book/index.html.twig', [
-            'controller_name' => 'BookController',
+            'books' => $bookRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_book_show', requirements: [ 'id' => '\d+' ], methods: ['GET'])]
+    public function show(?Books $book): Response
+    {
+        return $this->render('book/show.html.twig', [
+            'book' => $book
         ]);
     }
 }
