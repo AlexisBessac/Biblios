b99c2d162e696e39780cbc5007be64b2808e4c7a
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2025-03-26 14:56:45 +0100

    Mise en placer de la page d'accueil du projet Biblios

---
+++ src/Controller/BookController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BookController extends AbstractController
+{
+    #[Route('/book', name: 'app_book_index')]
+    public function index(BooksRepository $bookRepository, PaginatorInterface $paginator, Request $request): Response
+    {
+        // Récupère le numéro de page depuis la requête (par défaut : 1)
+        $page = $request->query->getInt('page', 1);
+        $limit = 4; // Nombre d'éléments par page
+
+        // Utilise la méthode du repository pour paginer
+        $books = $bookRepository->paginateBooks($page, $limit);
+
+        return $this->render('book/index.html.twig', [
+            'books' => $books,
+        ]);
+    }
+
+    #[Route('/book/{id}', name: 'app_book_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(?Books $book): Response
+    {
+        // Vérifie si le livre existe
+        if (!$book) {
+            throw $this->createNotFoundException('Le livre demandé n\'existe pas.');
+        }
+
+        return $this->render('book/show.html.twig', [
+            'book' => $book,
+        ]);
+    }
+}
