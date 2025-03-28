b82fd8992afd68203c6dff67bac337555e4ff5a6
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2025-03-28 17:51:58 +0100

    Mise en place des permissions sur les controllers

--- src/Controller/Admin/BooksController.php
+++ src/Controller/Admin/BooksController.php
@@ -10,7 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("IS_AUTHENTICATED")]
+#[IsGranted('ROLE_ADMIN')]
 #[Route('/admin/books')]
 final class BooksController extends AbstractController
 {
