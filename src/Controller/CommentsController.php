4206d7b80275982b296b253cb2737197f66668fd
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2025-04-02 18:23:43 +0200

    Changement dans le CommentsController

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 #[IsGranted("IS_AUTHENTICATED")]
 #[Route('/comments')]
